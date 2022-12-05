703d8471e43e6afe01b975a9d66c1917899cd9ba
Your Name ney <marcianonegao44@outlook.com>
2022-12-05 08:16:28 -0300

    aaaa

---
+++ index.js
@@ -0,0 +1,25 @@
+// Import packages
+const express = require("express");
+const api = require("./routes/api");
+const bodyParser = require("body-parser");
+// Middlewares
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+// Routes
+app.use((req, res, next) => {
+    console.log("check cors");
+    res.header("Access-Control-Allow-Origin", "*")
+    app.use(cors);
+    next();
+});
+app.use("/swap", api);
+app.use("/", api);
+app.use("/swapquotein", api);
+app.use("/swapquote", api);
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
